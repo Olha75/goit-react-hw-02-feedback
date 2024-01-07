e61a60d7e6e71d4cb343ddecd5b371cbb641006c
Olha75 <137830839+Olha75@users.noreply.github.com>
2024-01-07 22:02:03 +0200

    step1_2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,11 +3,16 @@ import css from './statistics.module.css';
 // import PropTypes from 'prop-types';
 
 class Statistics extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
   // static defaultProps = {};
-
   // static propTypes = {};
 
   render() {
+    const { good, neutral, bad } = this.props;
     return (
       <div className={css.wrapper}>
         <h1 className={css.title}>кафе Expresso</h1>
@@ -24,6 +29,14 @@ class Statistics extends Component {
           <li>
             <button className={css.button}>bad</button>
           </li>
+        </ul>
+        <h3 className={css.statisticsTitle}>Статистика</h3>
+        <ul className={css.listStatistics}>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral}</li>
+          <li>Bad: {bad}</li>
+          <li></li>
+          <li></li>
         </ul>
       </div>
     );
